e8df61beb9fdc3ac2fb4f77591a0ca6dc17eb7dd
mahendray-asu <mahendray@advsysunlimited.com>
2021-04-06 16:57:14 +0530

    Finished our LocalFramework

---
+++ LocalFramework_ios/LocalFramework_ios/LocalFramework_ios.h
@@ -0,0 +1,19 @@
+//
+//  LocalFramework_ios.h
+//  LocalFramework_ios
+//
+//  Created by Apple on 06/04/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalFramework_ios.
+FOUNDATION_EXPORT double LocalFramework_iosVersionNumber;
+
+//! Project version string for LocalFramework_ios.
+FOUNDATION_EXPORT const unsigned char LocalFramework_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalFramework_ios/PublicHeader.h>
+
+
---
+++ LocalFramework_ios/LocalFramework_ios/Service.swift
@@ -0,0 +1,25 @@
+//
+//  Service.swift
+//  LocalFramework_ios
+//
+//  Created by Apple on 06/04/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){
+        
+    }
+    
+    public static func doSomething()->String{
+        return "This is srevice function"
+        
+        
+    }
+    
+    
+}
+
